ce1281158ce1d5cc6c19bfd4ba09cbad2d8d7426
AzureAD\고명민 <miung222@gmail.com>
2024-08-08 22:22:32 +0900

    기본적인 샘플. Builder를 사용하지 않으면 오류 발생하는 부분에서 오래걸림.
    네비게이션을 위해 필요한 context를 얻기 위해 Builder 위젯을 사용한다.
    Builder를 사용하지 않으면 오류가 발생하는 이유.
    AppBar의 actions 속성에서 전달된 context가 Scaffold의 하위 위젯이 아니라 MaterialApp의 하위 위젯으로 간주되기 때문.
    이 경우 context가 Navigator를 포함하지 않아서 네비게이션 작업을 할 수 없다.
    Builder를 사용하면 Builder 위젯이 새로운 BuildContext를 생성하고,
    이 context는 Scaffold 내에 위치하게 되어 Navigator를 포함하게 된다.
    따라서, 네비게이션 작업이 정상적으로 동작.

---
+++ lib/custom_search_delegate.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchDelegate extends SearchDelegate<String> {
+  final List<String> searchList = [
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Date",
+    "Fig",
+    "Grapes",
+    "Kiwi",
+    "Lemon",
+    "Mango",
+    "Orange",
+    "Papaya",
+    "Raspberry",
+    "Strawberry",
+    "Tomato",
+    "Watermelon",
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () => Navigator.of(context).pop(),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final List<String> searchResults = searchList
+        .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    return ListView.builder(
+      itemCount: searchResults.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(searchResults[index]),
+          onTap: () {
+            close(context, searchResults[index]);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final List<String> suggestionList = query.isEmpty
+        ? []
+        : searchList
+            .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(suggestionList[index]),
+          onTap: () {
+            query = suggestionList[index];
+          },
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_search_delegate_fundamentals/custom_search_delegate.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Flutter SearchDelegate Fundamentals'),
+          actions: [
+            Builder(
+              builder: (context) => IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () async {
+                  showSearch(
+                    context: context,
+                    delegate: CustomSearchDelegate(),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
